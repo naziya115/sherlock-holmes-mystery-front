f8e0e4951b0be00ee51b05c6df41b48281f68fa6
naziya115 <naziyazhanabayeva@gmail.com>
2023-07-13 21:44:18 +0600

    story page

--- app/stories/stories.jsx
+++ app/stories/stories.jsx
@@ -1,6 +1,7 @@
+// Import the necessary modules
 'use client'
 import axios from 'axios';
-import Link from "next/link";
+import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 
 const Stories = () => {
@@ -17,50 +18,48 @@ const Stories = () => {
 
   const fetchStories = async () => {
     try {
-      const res = await axios.get("http://localhost:8000/stories/", {
+      const res = await axios.get('http://localhost:8000/stories/', {
         method: 'GET',
         headers: {
-          "accept": "application/json",
-          "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NGFlMzExZGRkMzQ5NmM4NDYyZDU2M2IiLCJleHAiOjE2ODk4ODE4MTF9.78MPFuUWikAHKnovzVzj3L-3LWkgz-JMwqXIUuytRsQ`
+          accept: 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
         },
       });
 
       const stories_inp = res.data.stories;
       setStories(stories_inp);
       setLoading(false);
-      console.log("stories:", stories_inp);
+      console.log('stories:', stories_inp);
     } catch (error) {
-      console.error("Error fetching stories:", error);
+      console.error('Error fetching stories:', error);
       setLoading(false);
     }
   };
 
   if (loading) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   if (!stories) {
-    return <div>No stories found.</div>; 
+    return <div>No stories found.</div>;
   }
 
   return (
-       <div className="flex flex-col items-center">
-        <h1>Hello world</h1>
-    {
-      stories.map((story) => (
+    <div className="flex flex-col items-center">
+      <h1>Hello world</h1>
+      {stories.map((story) => (
         <Link href={`/stories/${story._id}`} key={story._id}>
           <div className="flex flex-col items-center mb-4">
             <div className="w-full max-w-screen-xl p-6 bg-[#F9F9F9] border border-gray-200 rounded-lg shadow hover:bg-[#F7F7F7]">
               <h5 className="font-semibold mb-2 text-2xl text-black">{story.title}</h5>
               <p className="mb-2 text-lg text-black">{story.content.substring(0, 50)}</p>
+              <p>{story.user_id}</p>
             </div>
           </div>
         </Link>
-      ))
-    }
-    
-  </div>
+      ))}
+    </div>
   );
 };
 
-export default Stories
+export default Stories;
